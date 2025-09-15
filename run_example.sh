f52b64ad7c2ea1860fbd02f2ed5b4dcc93c8f0aa
Chataing Thibaut <chataint@gpu002.bamboo>
2025-09-15 15:52:30 +0200

    final hot fix

--- run_example.sh
+++ run_example.sh
@@ -1,38 +1,38 @@
 #!/bin/bash
 #SBATCH --cpus-per-task=24
 #SBATCH --job-name=rtmpose
-#SBATCH --chdir=/home/share/schaer2/idtracking_keypoint
+#SBATCH --chdir=/home/share/schaer2/idtracking
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
 #SBATCH --time=0-12:00:00
-#SBATCH --partition=shared-gpu,private-schaer-gpu
+#SBATCH --partition=public-gpu,shared-gpu
 #SBATCH --mem=64G
 #SBATCH --gpus=1
-#SBATCH --output=logs/slurm-%J.out
+#SBATCH --output=logs/rtmpose-%J.out
 #SBATCH --mail-type=END,FAIL
 
 echo `pwd -P`
 echo `nvidia-smi -L`
 
 # Execution parameters
-PROJROOT=/home/share/schaer2/idtracking_keypoint
+PROJROOT=/home/share/schaer2/idtracking
 
 # Load environment
 module load GCCcore/10.2.0 Python/3.8.6 cuDNN/8.9.2.26-CUDA-12.1.1
-source $PROJROOT/vitpose_env/bin/activate
+source $PROJROOT/vitpose_sam2/vitpose_env/bin/activate
 
 ##### Parameters
 # Adapt those to your setup
 
 # Common paths
-OUTPUT_DIR="/home/share/schaer2/idtracking_keypoint/output"
+OUTPUT_DIR="/home/share/schaer2/idtracking/test"
 
 # Full paths to video files
-VIDEO_PATH_USED_IN_SAM2=/home/share/schaer2/idtracking_keypoint/input/bedroom.mp4   # Here is the video used to extract the mask. We need it to normalize the bounding boxes
-INPUT_VIDEO_PATH_FOR_SKELETON=/home/share/schaer2/idtracking_keypoint/input/bedroom.mp4  # Here is the video used for skeleton extraction. The size of frame can be different but the fps NOT.
+VIDEO_PATH_USED_IN_SAM2=/home/share/schaer2/idtracking/test/bedroom.mp4   # Here is the video used to extract the mask. We need it to normalize the bounding boxes
+INPUT_VIDEO_PATH_FOR_SKELETON=/home/share/schaer2/idtracking/test/bedroom.mp4  # Here is the video used for skeleton extraction. The size of frame can be different but the fps NOT.
 
 # Mask to BBox parameters
-MASK_JSON="/home/share/schaer2/idtracking_keypoint/output/bedroom_mask_0-end.json"
+MASK_JSON="/home/share/schaer2/idtracking/test/bedroom_mask_0-200.json"
 
 
 
@@ -61,7 +61,7 @@ BBOX_OUTPUT_FILE="${OUTPUT_DIR}/${BBOX_FILENAME}"
 
 
 VITPOSE_SAM2_DIR="${PROJROOT}/vitpose_sam2"
-VITPOSE_CONFIG_DIR="${PROJROOT}/vitpose_config"
+VITPOSE_CONFIG_DIR="${PROJROOT}/pose_model_config"
 
 # MMPose parameters
 # POSE_CONFIG="${VITPOSE_SAM2_DIR}/mmpose/configs/body_2d_keypoint/topdown_heatmap/coco/td-hm_ViTPose-huge_8xb64-210e_coco-256x192.py"
